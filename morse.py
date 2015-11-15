0056e6f5a775723d2f37e29ebefeedfecb11cad2
root <root@ubilinux.(none)>
2015-11-15 10:31:24 +0000

    morse unix sockets

--- morse.py
+++ morse.py
@@ -1,49 +1,58 @@
-import mraa, time
+import mraa, time, socket, sys
 
-inputString = "SOS we want food"
-morseString = ''
-valueString = ''
 dotLength = 0.08
 dashLength = dotLength * 3
 wordSpacing = dotLength * 7
-words = inputString.split(' ')
 
-pin = mraa.Gpio(2)
+pin = mraa.Gpio(3)
+#pin.period_us(700)
+#pin.enable(True)
 pin.dir(mraa.DIR_OUT)
+loud = 1
+light = mraa.Aio(0)
 
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+server_address = './win98'
+sock.connect(server_address)
 
 def beep(slt):
-    pin.write(1)
+    pin.write(loud)
     time.sleep(slt)
     pin.write(0)
     time.sleep(dotLength)
 
+def beepm(inputString):
+	words = inputString.split(' ')
+	morseString = ''
+	valueString = ''
+	for w in words:
+	    for c in w.upper():
+		q = '_ETIANMSURWDKGOHVF_L_PJBXCYZQ__54_3___2__+____16=/_____7___8_90'.find(
+		    c)
+		s = ''
+		while q:
+		    s = '-.'[q % 2] + s
+		    q = ~-q // 2
+		morseString += (['/', '--..--', '..--..', '.-.-.-', ''][' ,?.'.find(c)] + s +' ')
+	    morseString += '\n'
 
-for w in words:
-    for c in w.upper():
-        q = '_ETIANMSURWDKGOHVF_L_PJBXCYZQ__54_3___2__+____16=/_____7___8_90'.find(
-            c)
-        s = ''
-        while q:
-            s = '-.'[q % 2] + s
-            q = ~-q // 2
-        morseString += (['/', '--..--', '..--..', '.-.-.-', ''][' ,?.'.find(c)] + s +' ')
-    morseString += '\n'
 
+	for c in morseString:
+	    if c == '.':
+		beep(dotLength)
+	    elif c == '-':
+		beep(dashLength)
+	    elif c == '\n':
+		time.sleep(wordSpacing)
+	    elif c == ' ':
+		time.sleep(dotLength * 5)
 
-for c in morseString:
-    if c == '.':
-        beep(dotLength)
-    elif c == '-':
-        beep(dashLength)
-    elif c == '\n':
-        time.sleep(wordSpacing)
-    elif c == ' ':
-	time.sleep(dotLength * 5)
 
 	
-print (valueString.strip( ',' ))
-
-
+while 1:
+	data = sock.recv(1)
+	print data
+	beepm(data)
 
 
